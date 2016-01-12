192c1bc4461bed92d1c9c789a20e4d58fdc916b9
David Carpenter <caprenter@gmail.com>
2016-01-12 14:16:39 +0000

    [#64] Fixes shortcode displaying before main body text
    Note that the function called by the shortcode should never produce
    output of any kind.
    https://codex.wordpress.org/Function_Reference/add_shortcode
    
    So turned the output of the function to return $html

--- wp-content/plugins/threesixty_salesforce_data/threesixty_saleseforce_data.php
+++ wp-content/plugins/threesixty_salesforce_data/threesixty_saleseforce_data.php
@@ -16,18 +16,20 @@ function list_datasets($atts, $content=null) {
                 set_transient( 'dcat-json-data', $datasets,60*60);
         }
         $dataset_list = json_decode(wp_remote_retrieve_body($datasets));
-        print '<table class="data-table tablesorter"><thead><tr><th class="sortless {sorter: false}">Logo</th><th class="header headerSortUp">Organisation</th><th class="header">Data</th></tr></thead><tbody>';
+        $html =  '<table class="data-table tablesorter"><thead><tr><th class="sortless {sorter: false}">Logo</th><th class="header headerSortUp">Organisation</th><th class="header">Data</th></tr></thead><tbody>';
         foreach($dataset_list as $dataset) {
-            print '<tr>';
+            $html .= '<tr>';
             if($dataset->publisher->logo != "") {
-                print '<td class="logo-cell"><img src="'. esc_url( $dataset->publisher->logo) .'" width="150" height="150", alt="'. esc_html( $dataset->publisher->name ) .' logo"/></td>';
+                $html .= '<td class="logo-cell"><img src="'. esc_url( $dataset->publisher->logo) .'" width="150" height="150", alt="'. esc_html( $dataset->publisher->name ) .' logo"/></td>';
             } else {
-                print '<td class="logo-cell">&nbsp;</td>';
+                $html .= '<td class="logo-cell">&nbsp;</td>';
             }
-            print '<td class="logo-cell">'. esc_html( $dataset->publisher->name) .'</td>';
-            print '<td class="logo-cell">'. esc_html( $dataset->publisher->name) .':<br/><a href="'. esc_url( $dataset->distribution[0]->downloadURL ) .'">'. esc_html( $dataset->distribution[0]->title ) .'</a></td>';
-            print '</tr>';
+            $html .= '<td class="logo-cell">'. esc_html( $dataset->publisher->name) .'</td>';
+            $html .= '<td class="logo-cell">'. esc_html( $dataset->publisher->name) .':<br/><a href="'. esc_url( $dataset->distribution[0]->downloadURL ) .'">'. esc_html( $dataset->distribution[0]->title ) .'</a></td>';
+            $html .= '</tr>';
         }
-        print '</tbody></table>';
+        $html .= '</tbody></table>';
+        
+        return $html;
 }
 add_shortcode('datasets', 'list_datasets');
