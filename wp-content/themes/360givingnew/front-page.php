cced192df5d86519347026427a03776d573be3c8
David Kane <david@dkane.net>
2020-02-18 16:51:10 +0000

    make sure front page always displays content from blog

--- wp-content/themes/360givingnew/front-page.php
+++ wp-content/themes/360givingnew/front-page.php
@@ -1,4 +1,38 @@
-<?php get_header(); ?>
+<?php
+    $posts_on_frontpage = 5;
+
+    /** Grab the sticky post ID's */
+    $sticky = get_option('sticky_posts');
+
+    /** Query the sticky posts */
+    $args = array(
+        'post__in'          => $sticky,
+        'posts_per_page'    => $posts_on_frontpage,
+        'post_type'         => 'post'
+    );
+    $sq = new WP_Query($args);
+
+    /** Count the number of post returned by this query */
+    $sq_count = $sq->post_count;
+
+    /** Check to see if any non-sticky posts need to be output */
+    if($sq_count < $posts_on_frontpage) :
+
+        $num_posts = $posts_on_frontpage - $sq_count;
+
+        /** Query the non-sticky posts */
+        $sticky = get_option('sticky_posts');
+        $args = array(
+            'post__not_in'      => $sticky,
+            'posts_per_page'    => $num_posts,
+            'post_type'         => 'post'
+        );
+        $nq = new WP_Query($args);
+        $sq->posts = array_merge($nq->posts, $sq->posts);
+        $sq->post_count += $nq->post_count;
+
+    endif;
+?><?php get_header(); ?>
 <div class="layout layout--single-column">
     <?php get_template_part('components/header'); ?>
     <main class="layout__content">
@@ -15,31 +49,34 @@
 
         <?php get_template_part('components/front-page-numbers') ?>
 
-        <?php if ( have_posts() ) : ?>
-        <?php $count = 0; ?>
         <section class="cards-section">
             <h2 class="cards-section__heading">From the Blog</h2>
             <ul class="card-list">
-                <?php while ( have_posts() & $count < 5 ) : the_post(); ?>
+                <?php if ( $sq->have_posts() ) : ?>
+                <?php $count = 0; ?>
+                <?php while ( $sq->have_posts() & $count < $posts_on_frontpage ) : $sq->the_post(); ?>
                 <li class="card-list__item">
                     <?php get_template_part('components/post-card'); ?>
                 </li>
                 <?php $count++; ?>
                 <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+                <?php endif; ?>
             </ul>
             <div class="align-center">
                 <a href="#" class="button button--teal">More from the blog</a>
             </div>
         </section>
-        <?php endif; ?>
 
+        <?php if ( get_option( 'show_on_front' ) == 'page' & have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
         <div class="base-section">
-            <div class="base-card base-card--none" style="height: 500px; padding: 80px;">
-                <h1 style="opacity: .3;">INSERT HTML HERE</h1>
+            <div class="base-card base-card--none">
+                <?php the_content(); ?>
             </div>
         </div>
-
-
+        <?php endwhile; ?>
+        <?php endif; ?>
 
     </main>
     </main>
