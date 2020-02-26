c6bda5e3ea12dc15e9c6bb0bbbdcd42862085147
David Kane <david@dkane.net>
2020-02-26 17:14:13 +0000

    add accordian block

--- wp-content/themes/360givingnew/functions/gutenberg.php
+++ wp-content/themes/360givingnew/functions/gutenberg.php
@@ -19,6 +19,27 @@ function tsg_person_register_block() {
 	register_block_type( 'tsg/person-block', array(
 		'editor_script' => 'tsg-person-block',
 	) );
- 
+	
 }
 add_action( 'init', 'tsg_person_register_block' );
+
+function tsg_accordion_register_block() {
+    
+	if ( ! function_exists( 'register_block_type' ) ) {
+		// Gutenberg is not active.
+		return;
+	}
+
+	wp_register_script(
+		'tsg-accordion-block',
+		get_template_directory_uri() . '/assets/js/accordion-block.js',
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ),
+		filemtime( plugin_dir_path( __FILE__ ) . '../assets/js/accordion-block.js' )
+	);
+
+	register_block_type( 'tsg/accordion-block', array(
+		'editor_script' => 'tsg-accordion-block',
+	) );
+	
+}
+add_action( 'init', 'tsg_accordion_register_block' );
