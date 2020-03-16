698ef918df8dee0d25c83537533dcdc38b110b2c
David Kane <david@dkane.net>
2020-03-16 15:39:18 +0000

    add two column template as option

--- wp-content/themes/360giving2020/two-columns.php
+++ wp-content/themes/360giving2020/two-columns.php
@@ -1,6 +1,16 @@
+<?php /* Template Name: Page with sidebar */ ?>
 <?php get_header(); ?>
 <?php if ( have_posts() ) : ?>
 <?php while ( have_posts() ) : the_post(); ?>
+<?php 
+    $parents = get_post_ancestors( get_the_ID() );
+    $top_level_post = ($parents) ? $parents[count($parents)-1]: get_the_ID();
+    $children = get_children(array(
+        "post_parent"=>$top_level_post,
+        "post_type"=>'page',
+        "post_status"=>'publish'
+    )); 
+?>
 <div class="layout <?php if(!$children): ?>layout--single-column<?php else: ?>layout--two-columns<?php endif; ?>">
     <?php get_template_part('components/header'); ?>
     <main class="layout__content">
@@ -16,6 +26,8 @@
             </div>
         </div>
     </main>
+    <?php include( locate_template( 'components/page-sidebar.php', false, false ) ); ?>
+    <?php get_template_part('components/page-sidebar'); ?>
     <?php get_template_part('components/footer'); ?>
 </div>
 <?php endwhile; ?>
