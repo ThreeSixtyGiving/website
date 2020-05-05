c68d13a565d2bce83d5f70bcd2f3822e1e6e9540
David Kane <david@dkane.net>
2020-05-05 17:41:35 +0100

    update with latest styles

--- wp-content/themes/360giving2020/index.php
+++ wp-content/themes/360giving2020/index.php
@@ -6,12 +6,13 @@
         <?php if ( have_posts() ) : ?>
             <?php while ( have_posts() ) : the_post(); ?>
             <div class="prose prose--wp">
-                <section class="prose__section prose__intro">
-                    <h1 class="h1"><?php the_title(); ?></h1>
-                    <div class="box">
-                        <p>By <?php the_author_posts_link(); ?></p>
-                        <p><time datetime="<?php the_time('c'); ?>"><?php the_time('F j, Y'); ?></time></p>
-                    </div>
+                <?php if(has_tag()): ?><h2 class="prose__brow"><?php the_tags(''); ?></h2><?php endif; ?>
+                <h1 class="prose__h1-long"><?php the_title(); ?></h1>
+                <p class="prose__author">
+                    By <?php the_author_posts_link(); ?>
+                    <time datetime="<?php the_time('c'); ?>"><?php the_time('F j, Y'); ?></time>
+                </p>
+                <section class="prose__section">
                     <?php the_content(); ?>
                 </section>
             </div>
