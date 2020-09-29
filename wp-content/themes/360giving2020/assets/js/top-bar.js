2cad948624447b08fc042a1a709084a648c93129
David Kane <david@dkane.net>
2020-09-29 15:41:40 +0100

    remove console.log

--- wp-content/themes/360giving2020/assets/js/top-bar.js
+++ wp-content/themes/360giving2020/assets/js/top-bar.js
@@ -1,6 +1,5 @@
 /* Top bar */
 document.addEventListener('DOMContentLoaded', function (event) {
-    console.log(document.querySelectorAll('.top-bar__menu-trigger, .off-canvas-menu__trigger'));
     document.querySelectorAll('.top-bar__menu-trigger, .off-canvas-menu__trigger').forEach(function (el) {
         el.onclick = function () {
             const offCanvasMenu = document.querySelector('.off-canvas-menu');
