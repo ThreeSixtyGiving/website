1b92ccbef7787e3fc2b7a529960b8a40b784647d
David Kane <david@dkane.net>
2021-07-20 12:21:09 +0100

    front page ux

--- wp-content/themes/360giving2020/components/front-page-numbers.php
+++ wp-content/themes/360giving2020/components/front-page-numbers.php
@@ -5,7 +5,7 @@
             <div class="base-card base-card--spacious base-card--teal">
                 <div class="base-card__content">
                     <p class="base-card__title"><?php echo tsg_number_format(get_theme_mod( 'tsg_funder_count', TSG_DEFAULTS['funder_count'] )); ?></p>
-                    <p class="base-card__text"><a href="https://grantnav.threesixtygiving.org/">Funders</a></p>
+                    <p class="base-card__text">Funders</p>
                 </div>
             </div>
         </div>
@@ -13,7 +13,7 @@
             <div class="base-card base-card--spacious base-card--teal">
                 <div class="base-card__content">
                     <p class="base-card__title"><?php echo tsg_number_format(round(get_theme_mod( 'tsg_grant_count', TSG_DEFAULTS['grant_count'] ), -3)); ?></p>
-                    <p class="base-card__text"><a href="https://grantnav.threesixtygiving.org/">Grants</a></p>
+                    <p class="base-card__text">Grants</p>
                 </div>
             </div>
         </div>
@@ -21,9 +21,13 @@
             <div class="base-card base-card--spacious base-card--teal">
                 <div class="base-card__content">
                     <p class="base-card__title"><small>£</small><?php echo tsg_number_format( get_theme_mod( 'tsg_grant_amount', TSG_DEFAULTS['grant_amount'] )); ?></p>
-                    <p class="base-card__text"><a href="https://grantnav.threesixtygiving.org/">of Grants Data</a></p>
+                    <p class="base-card__text">of Grants Data</p>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="spacer-3"></div>
+    <div style="text-align: center;">
+        <a class="button button--orange" style="text-align: center;" href="https://grantnav.threesixtygiving.org/">Explore these grants in GrantNav</a>
     </div>
 </section>
