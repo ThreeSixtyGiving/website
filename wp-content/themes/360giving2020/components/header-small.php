1c6aff5321a72e0905a4ffc5985f65f22a651666
David Kane <david@dkane.net>
2023-06-13 14:44:37 +0100

    Update header-small.php

--- wp-content/themes/360giving2020/components/header-small.php
+++ wp-content/themes/360giving2020/components/header-small.php
@@ -1,14 +1,3 @@
-<?php 
-    $tsg_page_kicker = get_post_meta(get_the_ID(), 'tsg_page_kicker', true);
-    $tsg_page_blurb  = get_post_meta(get_the_ID(), 'tsg_page_blurb',  true);
-    if ( is_category() ){
-        $tsg_page_kicker = single_cat_title( '', false );
-        $tsg_page_blurb  = category_description();
-    } elseif( is_home() ){
-        $tsg_page_kicker = 'Ideas & updates';
-        $tsg_page_blurb  = 'Our latest news, blogs and events where you can find out more about our work and stories of how open grants data is being used';
-    }
-?>
 <header class="layout__header">
     <div class="nav-bar">
         <a class="nav-bar__home-button" href="<?php echo get_home_url(); ?>">
